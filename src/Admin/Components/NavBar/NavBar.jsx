51118f9b2370e038800a099840b9ac6cb0cac25a
Yordan Gamboa Laina <ygamboalh@gmail.com>
2023-06-25 16:57:24 -0400

    Admin menu displaying better
    El menu del admin se muestra correctamente

--- src/Admin/Components/NavBar/NavBar.jsx
+++ src/Admin/Components/NavBar/NavBar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
   return (
-    <nav className="bg-white nav-bar fixed w-screen border-gray-200 dark:bg-gray-900">
+    <nav className="bg-white nav-bar z-10 fixed w-screen border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center">
           <div className="logo-admin"></div>
--- src/Admin/Components/PropertyListActive/property-list-active.jsx
+++ src/Admin/Components/PropertyListActive/property-list-active.jsx
@@ -2,7 +2,7 @@ import PropertiesList from "./properties";
 
 const PropertyListActive = () => {
   return (
-    <div className="overflow-x-auto mx-8 shadow-md sm:rounded-lg">
+    <div className="z-0 overflow-x-auto mx-8 shadow-md sm:rounded-lg">
       <table className="w-full mt-1 text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"></thead>
         <tbody>
