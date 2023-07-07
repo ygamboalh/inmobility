36847b382f2104c9bbd7d8f3a0364e66e4c726ac
Yordan Gamboa Laina <ygamboalh@gmail.com>
2023-07-07 17:41:12 -0400

    Updated investor

--- src/components/Investor/investor.jsx
+++ src/components/Investor/investor.jsx
@@ -5,17 +5,19 @@ const Investor = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col h-screen px-12 div-welcome text-center sm:px-10 md:px-6 justify-center items-center">
-      <div className="justify-center items-center mb-4 logo"></div>
-      <div className="flex-5 lg:w-96 md:w-96 text-white">
-        <label className="mb-4 font-bold text-xl">SISTEMA CIC</label>
+    <section className="h-full max-h-[540px]">
+      <div className="flex flex-col h-screen px-12 div-welcome text-center sm:px-10 md:px-6 justify-center items-center">
+        <div className="justify-center items-center mb-4 logo"></div>
+        <div className="flex-5 lg:w-96 md:w-96 text-white">
+          <label className="mb-2 font-bold text-xl">SISTEMA CIC</label>
+        </div>
+        <div className="flex-5 lg:my-2 lg:w-96 md:w-96 mb-6 text-white">
+          <span className="">
+            Estamos desarrollando esta sección para inversionistas
+          </span>
+        </div>
       </div>
-      <div className="flex-5 lg:my-5 lg:w-96 md:w-96 my-6 text-white">
-        <span className="">
-          Estamos desarrollando esta sección para inversionistas
-        </span>
-      </div>
-    </div>
+    </section>
   );
 };
 
