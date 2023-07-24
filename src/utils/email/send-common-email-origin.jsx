9f8aa73624205f2a3240d1abb9642eaf1cead2d3
Yordan Gamboa Laina <ygamboalh@gmail.com>
2023-07-24 08:20:53 -0400

    key removed

--- src/utils/email/send-common-email-origin.jsx
+++ src/utils/email/send-common-email-origin.jsx
@@ -20,8 +20,7 @@ function enviarCorreoComunOrigen(destinatario, body, asunto) {
       },
       {
         headers: {
-          "api-key":
-            "xsmtpsib-f95dfce070bb4651191512c4e66266afacaefda02c471d0d2cc95ef01f0d8a07-EWnZ8p62BFgqDPUj",
+          "api-key": "",
         },
       }
     )
