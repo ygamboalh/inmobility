4276d63be67f252e8eae240b648397230061036a
Yordan Gamboa Laina <ygamboalh@gmail.com>
2023-08-01 08:15:05 -0400

    key removed

--- src/utils/email/send-email.jsx
+++ src/utils/email/send-email.jsx
@@ -17,8 +17,7 @@ function enviarCorreo(destinatario, template) {
       },
       {
         headers: {
-          "api-key":
-            "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",
+          "api-key": "",
         },
       }
     )
