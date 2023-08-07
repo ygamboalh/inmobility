94a7a36eb35aaaca238cf3768dce5834489b5201
Yordan Gamboa Laina <ygamboalh@gmail.com>
2023-08-07 10:32:43 -0400

    Express running on 80

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs'); 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 const indexPath  = path.resolve(__dirname, '..', 'build', 'index.html');
 
 // static resources should just be served as they are
