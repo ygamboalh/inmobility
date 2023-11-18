a891a0fd71d76218edf82796956055f456b7e287
Yordan Gamboa Laina <ygamboalh@gmail.com>
2023-11-18 15:19:43 -0500

    Build 18-11 2

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,7 +1,10 @@
 import { message } from "antd";
 import AxiosInstance from "./AxiosInstance"
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 export const authUser = async (userData) => {
+  
   const { data } = await AxiosInstance.post('/auth/local', userData).catch((error) => {
     
     const err = error.response.data.error.message;
@@ -11,8 +14,31 @@ export const authUser = async (userData) => {
   });   
   
   if (data.user.isLoggedIn === true) {
-    message.error("¡Ya usted tiene una sesión abierta!");
-    return null;
+    const MySwal = withReactContent(Swal);
+  
+    MySwal.fire({
+      title: "Ya usted tiene una sesión abierta ¿Desea cerrarla?",
+      showDenyButton: true,
+      confirmButtonText: "Sí, deseo cerrarla",
+      confirmButtonColor: "#1863e4",
+      denyButtonText: `No`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const response = AxiosInstance.put(`/users/${data.user.id}`, {
+          isLoggedIn: false 
+       })
+          .then((res) => {
+            if (res.status === 200) {
+              message.info("¡La sesión fue cerrada correctamente!");
+              window.location.reload(true);
+              return null;
+           }
+         })
+         .catch((err) => {
+          message.error("¡La sesión no se puedo cerrar!");
+         });
+      }
+    });
   }
   else {
     const response = AxiosInstance.put(`/users/${data.user.id}`, {
